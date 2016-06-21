9a186a33ab2b553a6a1483837efc30332cbdba80
Karlis Lauva <k@karlis.me>
2016-06-21 14:07:15 -0600

    bump version

--- project.clj
+++ project.clj
@@ -1,24 +1,16 @@
-(defproject fullcontact/full.rabbit "0.9.1-SNAPSHOT"
+(defproject fullcontact/full.rabbit "0.10.0-SNAPSHOT"
   :description "RabbitMQ sugar on top of langohr."
-
-  :url "https://github.com/fullcontact/full.monty"
-
+  :url "https://github.com/fullcontact/full.rabbit"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
-
   :deploy-repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
-
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.novemberain/langohr "3.2.0"
-                  :exclusions [cheshire]]
-                 [fullcontact/full.metrics "0.9.1-SNAPSHOT"]
-                 [fullcontact/full.json "0.9.1-SNAPSHOT"]
+                 [com.novemberain/langohr "3.2.0" :exclusions [cheshire]]
+                 [fullcontact/full.metrics "0.9.0"]
+                 [fullcontact/full.json "0.9.0"]
                  [fullcontact/full.async "0.9.0"]
-                 [fullcontact/full.core "0.9.1-SNAPSHOT"]]
-
+                 [fullcontact/full.core "0.9.0"]]
   :aot :all
-
   :plugins [[lein-midje "3.1.3"]]
-
   :profiles {:dev {:dependencies [[midje "1.7.0"]]}})
