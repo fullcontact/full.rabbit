15eca9e04a0b02b999eb7e1b6c7c42e79de766b0
Karlis Lauva <k@karlis.me>
2016-06-21 15:45:13 -0600

    upgrade to 0.10 stack

--- project.clj
+++ project.clj
@@ -5,12 +5,20 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :deploy-repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.novemberain/langohr "3.2.0" :exclusions [cheshire]]
-                 [fullcontact/full.metrics "0.9.0"]
-                 [fullcontact/full.json "0.9.0"]
+                 [fullcontact/full.metrics "0.10.0"]
+                 [fullcontact/full.json "0.10.0"]
                  [fullcontact/full.async "0.9.0"]
-                 [fullcontact/full.core "0.9.0"]]
+                 [fullcontact/full.core "0.10.0"]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
   :aot :all
   :plugins [[lein-midje "3.1.3"]]
   :profiles {:dev {:dependencies [[midje "1.7.0"]]}})
