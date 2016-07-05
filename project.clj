f05b802c7623061bdc8cda73a79eac09b26f3f48
Karlis Lauva <k@karlis.me>
2016-07-05 13:01:19 -0600

    Version 0.10.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.rabbit "0.10.3"
+(defproject fullcontact/full.rabbit "0.10.4-SNAPSHOT"
   :description "RabbitMQ sugar on top of langohr."
   :url "https://github.com/fullcontact/full.rabbit"
   :license {:name "Eclipse Public License - v 1.0"
